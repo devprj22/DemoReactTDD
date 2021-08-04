2fc19696a98ab0b7ec03d5955249b2e806f98969
prj <shdwmaster632@gmail.com>
2021-08-04 22:20:28 +0530

    Added button toggling in the button click test

--- src/App.test.js
+++ src/App.test.js
@@ -8,14 +8,18 @@ test('button has correct initial color', () => {
   expect(colorButton).toHaveStyle({backgroundColor : 'red'});
 });
 
-test('button turns blue when clicked', () => {
+test('button turns blue when clicked and then back to red', () => {
 
   render(<App />);
   const colorButton = screen.getByRole('button', {name : 'Change to blue'});
+  
   fireEvent.click(colorButton);
-
   expect(colorButton).toHaveStyle({backgroundColor : 'blue'});
   expect(colorButton.textContent).toBe('Change to red');
+
+  fireEvent.click(colorButton);
+  expect(colorButton).toHaveStyle({backgroundColor : 'red'});
+  expect(colorButton.textContent).toBe('Change to blue');
 });
 
 test('initial conditions', () => {
